79722dc9eedcd0cd08e9c0a956ba8ce560b00747
Brian Pease <pease.brian@gmail.com>
2020-12-27 15:28:06 -0600

    message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,9 +37,7 @@ export class AppComponent {
     searchTerm = searchTerm.toLowerCase();
     for(let i=0; i < this.sourceList.length; i++) {
        let name:string = this.sourceList[i].name.toLowerCase();
-       let type:string = this.sourceList[i].type.toLowerCase();
-       let orbitType:string = this.sourceList[i].orbitType.toLowerCase();
-       if (name.indexOf(searchTerm) >= 0 || type.indexOf(searchTerm) >= 0 || orbitType.indexOf(searchTerm) >= 0) {
+       if (name.indexOf(searchTerm) >= 0) {
           matchingSatellites.push(this.sourceList[i]);
        }
     }
